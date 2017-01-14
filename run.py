f80419f08ff67e5160ce397b6854f2e4c4fe8efb
Ubuntu <ubuntu@ip-172-31-21-242.us-west-2.compute.internal>
2017-01-14 18:28:06 +0000

    Adding requirement.txt, run.py

---
+++ run.py
@@ -0,0 +1,14 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def helloWorld():
+	return "Hello Everyone!"
+
+if __name__ == '__main__':
+	host = os.getenv('IP','0.0.0.0')
+	port = int(os.getenv('PORT',5000))
+	app.debug = True;
+	app.run(host=host, port=port)
